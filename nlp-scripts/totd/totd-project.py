6a021f05d5c4da3a93d6f1f9cdefe655e325c645
Salvatore Ventrone <ventrosky@gmail.com>
2017-03-04 10:55:14 +0100

    Markov 1
    Fix Markov 1 probability first word

--- nlp-scripts/totd/totd-project.py
+++ nlp-scripts/totd/totd-project.py
@@ -200,22 +200,26 @@ def maxProbMarkov0(lenCorpus, distrFreq, frasi):
     return topFrase, probFraseMax
 
 # Calcola Probabilita Frase Markov 1; bigramInfos = { bigramma : (F(u,v), F(u), F(v), P(v|u), P(u,v))}
-def probabilitaMarkov1(frase, bigramInfos): 
+def probabilitaMarkov1(lenCorpus, frase, bigramInfos): 
     probabilita = 1.0
     w1 = frase[0]
+    primaParola = True
     for tok in frase[1:]:
         w2 = tok
+        if primaParola:
+            probabilita = ( bigramInfos[(w1,w2)][2] *1.0 / lenCorpus*1.0) #calcolo prob prima parola
+            primaParola = False
         probabilitaBigramma = bigramInfos[(w1,w2)][3] # P(v|u)
         probabilita = probabilita * probabilitaBigramma #P(w1, w2,...,wn)=P(w1)*P(w2|w1)*P(w3|w2)*...*P(wn|wn-1)
         w1 = w2
     return probabilita
 
 # restituisce la frase Markov 1 con probabilità più alta
-def maxProbMarkov1(frasi, bigramInfos):
+def maxProbMarkov1(lenCorpus, frasi, bigramInfos):
     probFraseMax = 0
     topFrase = []
     for frase in frasi:
-        p = probabilitaMarkov1(frase, bigramInfos) # Probabilita Frase Markov 1 
+        p = probabilitaMarkov1(lenCorpus, frase, bigramInfos) # Probabilita Frase Markov 1 
         if p > probFraseMax:
             probFraseMax = p
             topFrase = frase
@@ -320,7 +324,7 @@ def main():
     print '-'*80
     infoBigrammi1 = infoBigrams(bigrams1, set(bigrams1), vocabFreq1) # dizionario con info bigrammi : (F(u,v), F(u), F(v), P(v|u), P(u,v))
     
-    topFrase1, probFraseMax1 = maxProbMarkov1(frasiMarkov1, infoBigrammi1) # frase Markov 1 con probabilità più alta    
+    topFrase1, probFraseMax1 = maxProbMarkov1(len(tokensText1), frasiMarkov1, infoBigrammi1) # frase Markov 1 con probabilità più alta    
     print " 2° Frase calcolata attraverso un modello di Markov di ordine 1:"
     print '-'*80
     print ' "'," ".join(topFrase1).encode('utf-8'),'"'
