6f453ef36f9df1f351c2d7fe16802bd994edf438
Salvatore Ventrone <ventrosky@gmail.com>
2019-03-26 10:38:01 +0100

    Update sentiment.py
    fix

--- nlp-scripts/sentiment.py
+++ nlp-scripts/sentiment.py
@@ -6,16 +6,16 @@ from rasa_nlu.model import Metadata
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from textblob import TextBlob
 
-#pipeline:      
-#- name: "sentiment.SentimentAnalyzer" 
+# pipeline name: "sentiment.sentiment_analyzer"
+# should be added to component_classes in registry.py
 
 class SentimentAnalyzer(Component):
     """A pre-trained sentiment component"""
-    name = "sentiment"
+    name = "sentiment.sentiment_analyzer"
     provides = ["entities"]
     requires = []
     defaults = {}
-    language_list = ["en"]
+    language_list = ["en", "it"]
 
     def __init__(self, component_config=None):
         super(SentimentAnalyzer, self).__init__(component_config)
@@ -29,23 +29,20 @@ class SentimentAnalyzer(Component):
                   "confidence": confidence,
                   "entity": "sentiment",
                   "extractor": "sentiment_extractor"}
-
         return entity
 
     def process(self, message, **kwargs):
         """Retrieve the text message, translate to italian, pass it to the classifier
             and append the prediction results to the message class."""
-		ita_message = message.text
+        ita_message = TextBlob(message.text)
         if(ita_message.detect_language()=="it"):
-            ita_message = TextBlob(message.text).translate(from_lang="it", to='en')
+            ita_message = ita_message.translate(from_lang="it", to='en')
         #ita_message.sentiment
         #ita_message.sentiment.polarity
         sid = SentimentIntensityAnalyzer()
         res = sid.polarity_scores(ita_message)
         key, value = max(res.items(), key=lambda x: x[1])
-
         entity = self.convert_to_rasa(key, value)
-
         message.set("entities", [entity], add_to_output=True)
 
     def persist(self, model_dir):
